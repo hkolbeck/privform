ce697300dba14f1534c7dcd619f6168e49e41370
Hannah Kolbeck <hskolbeck@gmail.com>
2023-01-21 13:06:52 -0800

    Composer formatting done minus header

--- site/src/crypto.js
+++ site/src/crypto.js
@@ -9,7 +9,7 @@ async function generateKeypair(password) {
     return {
         privkey: privateKey,
         pubkey: publicKeyArmored,
-        fingerprint: publicKey.keyPacket.getFingerprint() || "A999B7498D1A8DC473E53C92309F635DAD1B5517",
+        fingerprint: publicKey.keyPacket.getFingerprint(),
         accessKey: crypto.randomUUID()
     }
 }
